c0475e19777d2e54c3bfc1ffd74d05976010f9e9
evive82 <evodell@pm.me>
2020-05-08 20:25:58 -0700

    making it a little more concise

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,18 +1,15 @@
 def caesar_cipher string, shift = 0
-  alpha_cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  alpha_low = "abcdefghijklmnopqrstuvwxyz"
-  characters = string.chars
-  characters.map! do |letter|
+  ciphered = string.chars.map do |letter|
     unless letter.match?(/[A-Za-z]/)
       letter
     else
-      letter_case = alpha_cap.include?(letter) ? alpha_cap : alpha_low
+      letter_case = letter.ord < 97 ? ('A'..'Z').to_a : ('a'..'z').to_a
       index = letter_case.index(letter) + shift
       index = index > 25 || index < -26 ? (index % 26) : index
       letter_case[index]
     end
   end
-  characters.join
+  ciphered.join
 end
 
 puts caesar_cipher("What a string!", 5)
